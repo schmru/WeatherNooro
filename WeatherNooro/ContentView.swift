e83cb0a90ee36b43181dc886a6c1eb2124138026
schmru <miha.semrl@gmail.com>
2024-12-14 14:21:03 +0100

    Initial Commit

---
+++ WeatherNooro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherNooro
+//
+//  Created by Miha on 14/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherNooro/WeatherNooroApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNooroApp.swift
+//  WeatherNooro
+//
+//  Created by Miha on 14/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherNooroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
